33749b77481cc6e490e3d6d411adba48acb603ea
Nellie <fabybacenelson@gmail.com>
2020-03-04 15:14:11 -0700

    rendering input

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-$(document).ready(function(){
-    $(".input").focus(function(){
-        $(document).on('keypress',function(e) {
-            if(e.which == 13) {
-                alert('You pressed enter!');
+$(document).ready(function () {
+    $(".input").focus(function () {
+        $(document).on('keypress', function (e) {
+            if (e.which == 13) {
+                let inp = $(".input").val();
+                renderMsg("right", inp);
+                let res = getResponse(inp);
+                renderMsg("left", res);
+                $(".input").val("");
+
+                $(".response").animate({
+                    scrollTop: $(".response").get(0).scrollHeight
+                }, 1000);
             }
         });
     });
-    
+
 });
+
+function renderMsg(side, msg) {
+    let div = `<div class="${side}">${msg}</div>`;
+    $(".response").append(div);
+}
+
+function getResponse(input){
+    let res = "Sorry, I haven't programmed a response to that.";
+
+
+    
+    return res;
+}
